ae5b16ed045dbbbf0f5848bea15aa62abcfda4ec
Murugan V <visumurugan@gmail.com>
2023-04-15 01:07:24 +0530

    added-validations

--- src/app/text-area/text-area.component.ts
+++ src/app/text-area/text-area.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./text-area.component.scss']
 })
 export class TextAreaComponent {
+  isDisabled = true;
+  enteredText: string = '';
 
+  addNoteValidation(): void {
+    this.isDisabled = this.enteredText.trim() && this.enteredText ? false : true;
+  }
+
+  openDropdown(event: KeyboardEvent): void {
+    if(event.key === '@') {
+      //TODO
+    }
+  }
+
+  clearText(): void {
+    this.enteredText = '';
+    this.isDisabled = true;
+  }
 }
